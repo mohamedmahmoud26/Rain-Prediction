d5618b42d08c5b1777ebf9845907c70770beb178
Mahmoud Ebrahim <142084714+B-MEbrahim@users.noreply.github.com>
2025-05-10 20:13:05 +0300

    initial Home Page

--- app_src/Home.py
+++ app_src/Home.py
@@ -4,13 +4,6 @@ import pandas as pd
 import joblib 
 
 
-model_package = joblib.load(r"model\rainfall_model_package.joblib")
-model = model_package['model']
-
-df = pd.read_csv(r"data\test_data.csv")
-X = df.drop('RainTomorrow', axis=1)
-y = df['RainTomorrow']
-
 
 st.set_page_config(page_title="Rain Prediction", layout="wide")
 
